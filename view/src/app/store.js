63dc9e39f27eb33d4a5110e21de3e3ff46b41f57
dev <ygbe@hotmail.fr>
2022-11-29 20:58:23 +0000

    persistent login state

--- view/src/app/store.js
+++ view/src/app/store.js
@@ -1,8 +1,23 @@
 import {configureStore} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import thunk from 'redux-thunk';
 import authenticationReducer from './authenticationSlice.js';
 
+const reducers = combineReducers({
+    authentication: authenticationReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-    reducer: {
-        authentication: authenticationReducer
-    }
+    reducer: persistedReducer,
+    devTools: true,
+    middleware: [thunk],
 });
